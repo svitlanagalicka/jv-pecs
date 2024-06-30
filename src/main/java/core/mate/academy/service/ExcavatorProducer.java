0c7b78c73a44c1233b65ec1b25ba8489d6a15f43
Svitlana Galicka <svitlanakozak12332@gmail.com>
2024-06-30 13:07:44 +0300

    used List.of

--- src/main/java/core/mate/academy/service/ExcavatorProducer.java
+++ src/main/java/core/mate/academy/service/ExcavatorProducer.java
@@ -1,14 +1,11 @@
 package core.mate.academy.service;
 
 import core.mate.academy.model.Excavator;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExcavatorProducer implements MachineProducer<Excavator> {
     @Override
     public List<Excavator> get() {
-        List<Excavator> excavators = new ArrayList<>();
-        excavators.add(new Excavator());
-        return excavators;
+        return List.of(new Excavator());
     }
 }
